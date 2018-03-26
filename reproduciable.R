01f88ca4df4dd5e8f143f51188778b1db9ad04d4
Joe Zhu <sha.joe.zhu@gmail.com>
2018-03-26 13:57:53 +0100

    update

--- reproduciable.R
+++ reproduciable.R
@@ -4,12 +4,18 @@ library(dplyr)
 load("new_Table.Rdata")
 load("compiled_data.Rdata")
 
+cat("################################# abstract ##########################\n")
+cat("#####################################################################\n")
+cat("#####################################################################\n")
 ################## abstract
 groupByCountry = group_by(new.data, new.data$Population)
 summarise(groupByCountry, sum(Est_K>1)/length(Est_K))[,2] %>% range %>% cat("range",., "\n")
 
-print(cor.test(new_table$eff_k[new_table$nsam>15], new_table$pfpr_nonzero[new_table$nsam>15]))
-print(cor.test(new_table$relatedness[new_table$nsam>15], new_table$pfpr_nonzero[new_table$nsam>15]))
+good_table = filter(new_table, nsam > 15)
+print("effective k vs prevalence")
+cor.test(good_table$eff_k, good_table$pfpr_nonzero) %>% print(.)
+print("relatedness vs prevalence")
+cor.test(good_table$relatedness, good_table$pfpr_nonzero) %>% print(.)
 
 
 print(sum(new.data$Est_K>2)/sum(new.data$Est_K>1))
@@ -18,16 +24,18 @@ sum(new.data$cluster=="Sib")/sum(new.data$Est_K>1)
 
 sum(new.data$cluster%in%c("Sib", "HighSib"))/sum(new.data$Est_K>1)
 # 54%???
-sum(new.data$Est_K==2) / sum(new.data$Est_K>1)
+sum(new.data$Est_K==2) / sum(new.data$Est_K>1) %>%
 
-print("Unsure about 54%")
-!!!!!!!!!!!!!!!!!!!!!!!!!!
+cat("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Unsure about 54%\n")
+#!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-print("Unsure about 71%")
-!!!!!!!!!!!!!!!!!!!!!!!!!!
+cat("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Unsure about 71%\n")
+#!!!!!!!!!!!!!!!!!!!!!!!!!!
 # 71%???
 
-
+cat("################################# Validation ##########################\n")
+cat("#####################################################################\n")
+cat("#####################################################################\n")
 ################### Validation
 load("all.error.IBD.Rdata")
 load("all.error.nonIBD.Rdata")
@@ -35,13 +43,20 @@ load("all.error.nonIBD.Rdata")
 group_by(all.error, case_panel) %>% summarise(., mean(genotypError)) %>% print
 group_by(all.nonIBD.error, case_panel) %>% summarise(., mean(genotypError)) %>% print
 
+
+cat("################################# Figure2 ##########################\n")
+cat("#####################################################################\n")
+cat("#####################################################################\n")
 #################### Figure 2
 print( 1/(.25^2+.75^2) )
 
 print( 1/(.55^2+.45^2) )
 
+cat("################################# Results ##########################\n")
+cat("#####################################################################\n")
+cat("#####################################################################\n")
 #################### Results
-print(summary(new.data$Depth)%>% ceiling)
+print(summary(new.data$Depth)%>% round)
 
 print("% of mixture")
 summarise(groupByCountry, percent_mix = sum(Est_K>1)/length(Est_K)) %>% print
@@ -74,15 +89,17 @@ filter(complex_infection, group %in% levels(complex_infection$group)[c(4:7)])$re
 a = dual_infection%>% group_by(., Population) %>% summarise(., rho = mean(relatedness, na.rm=T))
 b = complex_infection%>% group_by(., Population) %>% summarise(., rho = mean(relatedness, na.rm=T))
 
-print("weighted linear model?????????")
-!!!!!!!!!!!!!!!!!!!!!!!!!!
+cat("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!weighted linear model?????????\n")
 cor.test(a$rho, b$rho)
+#!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-print("Overall, XXXX% of all mixed infections have 30% IBD")
+cat("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Overall, XXXX% of all mixed infections have 30% IBD\n")
 a = new.data$relatedness[new.data$Est_K>1] > .3
 print(sum(a, na.rm=T)/length(a))
 
-
+cat("################################# Results Characteristics  ##########################\n")
+cat("#####################################################################\n")
+cat("#####################################################################\n")
 ################################## Characteristics of mixed infection correlate with local prevalence
 print("prevalence range")
 range(new_table$pfpr_nonzero, na.rm=T)
@@ -91,9 +108,7 @@ filter(new_table, grepl("Asia", group))$pfpr_nonzero %>% mean(., na.rm=T) %>% ca
 
 filter(new_table, grepl("Africa", group))$pfpr_nonzero %>% mean(., na.rm=T) %>% cat("Africa prevalence", ., "\n")
 
-print("page 10 correlations")
-!!!!!!!!!!!!!!!
-good_table = filter(new_table, nsam > 15)
+cat("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!page 10 correlations \n")
 as = filter(good_table, grepl("Asia", group))
 af = filter(new_table, grepl("Africa", group))
 cor.test(good_table$pfpr_nonzero, good_table$eff_k)
@@ -106,6 +121,9 @@ cor.test(as$pfpr_nonzero, as$relatedness)
 cor.test(af$pfpr_nonzero, af$relatedness)
 
 
+cat("################################# Discussion  ##########################\n")
+cat("#####################################################################\n")
+cat("#####################################################################\n")
 ############################### Discussion
 summarise(groupByCountry, sum(Est_K>1)/length(Est_K))[,2] %>% range %>% cat("range",., "\n")
 print(sum(new.data$Est_K>2)/sum(new.data$Est_K>1))
@@ -114,7 +132,7 @@ vietnam = filter(new.data, Population=="Vietnam")
 
  sum(vietnam$Est_K>1) / 97
 sum(vietnam$cluster=="Sib") /sum(vietnam$Est_K>1)
-print("include high sib???")
+cat("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!include high sib??\n")
 sum(vietnam$cluster %in% c("Sib", "HighSib")) /sum(vietnam$Est_K>1)
 
 
